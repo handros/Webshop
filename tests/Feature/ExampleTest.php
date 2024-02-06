278644a918c5409135155c73380d4ccd556f96d9
andras-hernady <handris2001@gmail.com>
2024-02-06 02:38:46 +0100

    Test modify

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,10 @@ class ExampleTest extends TestCase
 
     //     $response->assertStatus(200);
     // }
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(302);
+    }
 }
