da098970798bc379a76a1d22d9c270a045b23ed5
andras-hernady <handris2001@gmail.com>
2024-11-07 05:19:27 +0100

    feat: answer to msges

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -9,6 +9,19 @@
                         <strong>{{ $message->sender_id === auth()->id() ? 'Ön' : $message->sender->name }} </strong>
                         <i class="fas fa-angle-right"></i> <strong>{{ $message->receiver_id === auth()->id() ? 'Ön' : $message->receiver->name }} </strong>
                         <em> - {{ $message->created_at->format('Y-m-d H:i') }}</em>
+                        @if(auth()->id() !== $message->sender->id)
+                            <a href="#messageForm" class="btn-sm btn-primary reply-btn"
+                                data-receiver="{{ $message->sender->id }}"
+                                data-receiver-name="{{ $message->sender->name }}">
+                                <i class="fas fa-reply"></i>
+                            </a>
+                        @elseif(auth()->id() !== $message->receiver->id)
+                            <a href="#messageForm" class="btn-sm btn-primary reply-btn"
+                                data-receiver="{{ $message->receiver->id }}"
+                                data-receiver-name="{{ $message->receiver->name }}">
+                                <i class="fas fa-reply"></i>
+                            </a>
+                        @endif
                     </summary>
                     <p>{{ $message->text }}</p>
                 </details>
